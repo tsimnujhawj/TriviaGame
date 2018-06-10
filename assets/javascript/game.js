8d3d5e759d6847a514bffe59dcbfa09a3ec03a3c
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-09 19:49:45 -0500

    started pseudo code, working out the timer atm

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+/* jQuery doc ready
+
+DOM html start game (click HERE to start)
+
+DOM start game event listener click function(){
+disable button
+startTimer()
+}
+
+function startTimer() {
+countdown timer for x amount of time
+DOM html the timer
+questions()
+}
+
+function questions()
+{
+    while (timer > 0)
+{
+DOM html random question and choices
+event listener click function()
+    {
+    var chosenClick = this event
+    if (chosenClick === this event)
+        {
+        win()
+        }
+        else if (chosenClick !== this event)
+        {
+        lose()
+        }
+    }
+}
+console.log("Test-End")
+}
+
+function win() {
+    wins++
+    DOM html ("you won!")
+    wait 5 seconds
+    startTimer()
+    return wins
+}
+
+function lose() {
+    losses++
+    DOM html ("you lost!")
+    wait 5 seconds
+    startTimer()
+    return losses
+}
+
+
+*/ // jQuery doc ready closing
